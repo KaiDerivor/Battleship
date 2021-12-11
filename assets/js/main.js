bdda9a17e5645d0a3f35f06bea794bb628199ea2
Dima Mychalchuk <dimamuchalchuk@gmail.com>
2021-12-11 17:37:44 +0300

    number ship is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,63 @@
 const startGame = {
   numberCol: 7,
   numberRow: 7,
-  firstShip: [],
-  secondShip: [],
-  thirdShip: [],
+  
+  firstShip:[],
+  secondShip:[],
+  thirdShip:[],
+  allPlaceShip:[],
+  allReserveShip:[],
+  defineNumberShip:1,
 
   defineShips: function () {
-    const colRow = this.verticalHorizontal();
-    if (colRow == 1) {
-      const positionX = Math.floor(Math.random() * (this.numberCol - 3));
-      const positionY = Math.floor(Math.random() * this.numberRow);
-      this.putShips(positionX, positionY, "x");
-    } else {
-      const positionX = Math.floor(Math.random() * (this.numberCol));
-      const positionY = Math.floor(Math.random() * (this.numberRow-3));
-      this.putShips(positionX, positionY, "y");
-    }
+    let reservePlace=0;
+    console.log(this.defineNumberShips+' Number ships');
+    const lengt=+this.defineNumberShip;
+    let cloneLength=0;
+    for(let i=0;i<= this.defineNumberShip;i++){
+        if(cloneLength<lengt){
+          switch (cloneLength) {
+            case 0:
+              reservePlace=4;
+              break;
+            case 1:case 2:
+              reservePlace=3;
+              break;
+              case 3:case 4:case 5:
+                reservePlace=2;
+                break;
+            default: reservePlace=1;
+              break;
+              
+          }
+          cloneLength++;
+          const colRow = this.verticalHorizontal();
+          if (colRow == 1) {
+            const positionX = Math.floor(Math.random() * (this.numberCol - reservePlace));
+            const positionY = Math.floor(Math.random() * this.numberRow);
+            this.putShips(positionX, positionY, "x",reservePlace);
+          } else {
+            const positionX = Math.floor(Math.random() * (this.numberCol));
+            const positionY = Math.floor(Math.random() * (this.numberRow-reservePlace));
+            this.putShips(positionX, positionY, "y",reservePlace);
+          }
+        }
+      }
+     
   },
   verticalHorizontal: function () {
     return Math.round(Math.random() * 100) % 2;
   },
-  putShips: function (x, y, flag) {
+  putShips: function (x, y, flag,reservePlace) {
     let startXY = [];
     if (flag === "x") {
-      for (let i = 0; i < 3; i++) {
+      for (let i = 0; i < reservePlace; i++) {
         x++;
         startXY.push(x + "" + y);
+        
       }
     } else {
-      for (let i = 0; i < 3; i++) {
+      for (let i = 0; i < reservePlace; i++) {
         y++;
         startXY.push(x + "" + y);
       }
@@ -41,44 +70,64 @@ const startGame = {
     for (const item of arr) {
       mainShip.push(item);
     }
-    if (this.firstShip.length <= 0) {
-      this.firstShip = mainShip;
-    } else if (this.secondShip.length <= 0) {
-      this.secondShip = mainShip;
-    } else {
-      this.thirdShip = mainShip;
-    }
-  },
+    this.allPlaceShip.push(mainShip);
+    // console.log(this.allPlaceShip);
+  }, 
+
+
 };
 const model = {
     counterHits:0,
     counterShip:0,
     allShip:[],
-  findShip: function (ships) {
-      let ship=[];
-      ship=ships['0'];
+    shipRemove:function(){
+      const ship=this.allShip;
       for (const id of ship) {
         const td=document.getElementById(id);
-        td.textContent='x';
+          td.textContent='';
+          td.style.backgroundColor='rgb(117, 233, 248)';
+        
       }
+      const tr=document.getElementsByClassName('field');
+      for (const id of tr) {
+         id.textContent='';
+       id.style.backgroundColor='rgb(117, 233, 248)';
+      } 
+    },
+  findShip: function (ships) {
+      let ship=ships;
+      for (const arr of ship) {
+        for (const id of arr) {
+          for (const index of id) {
+            const td=document.getElementById(index);
+          td.textContent='x';
+          }
+      }
+    }
   },
   placeShip:function(){
-    for (const xy of startGame.firstShip[0]) {
-        this.allShip.push(xy);
+    for (let i=0;i<startGame.allPlaceShip.length;i++) {
+        for (const index of startGame.allPlaceShip[i]) {
+          for(const id of index){
+             this.allShip.push(id);
+          }
+        }
     }
-    for (const xy of startGame.secondShip[0]) {
-        this.allShip.push(xy);
-    }for (const xy of startGame.thirdShip[0]) {
-        this.allShip.push(xy);
-    }
+    console.log('all ship'+this.allShip);
+    
   },
   updateHits:function(){
       const span=document.querySelector('.counterHits');
       span.textContent=this.counterHits;
   },
   updateShip:function(){
-    let counter=Math.ceil((this.allShip.length)/3);
-     
+   //  const lengthAllPlaceShip=startGame.allPlaceShip.length;     
+    let counter=0;
+    for (const indexArr of startGame.allPlaceShip) {
+       for (const id of indexArr) {
+      if(id.length) counter++;
+       }
+    }
     const numberShips=document.querySelector('.numberShip');
     numberShips.textContent=counter;
     if(counter===0){
@@ -87,14 +136,24 @@ const model = {
     }
   },
   isHit:function(id,e){
-    if(this.allShip.includes(id)){
-        this.allShip.splice(this.allShip.indexOf(id),1);
-        e.target.style.backgroundColor='yellow';
-        e.target.textContent="hit";
-    } else{
-        e.target.textContent="miss";
-        e.target.style.backgroundColor='green';
+    const lengthAllPlaceShip=startGame.allPlaceShip[0];     
+    for (let i=0;i<startGame.allPlaceShip.length;i++) {
+       for(let j=0;j<startGame.allPlaceShip[i].length;j++){
+         if(startGame.allPlaceShip[i][j].length){
+               if(startGame.allPlaceShip[i][j].includes(id)){
+                  startGame.allPlaceShip[i][j].splice(startGame.allPlaceShip[i][j].indexOf(id),1);
+                  e.target.style.backgroundColor='yellow';
+                  e.target.textContent="hit";
+                  return;
+              
+            }
+          
+        }
+       }
+     
     }
+    e.target.textContent="miss";
+    e.target.style.backgroundColor='green';
   },
   playAgain:function(){
       const bnt=document.querySelector('.reset');
@@ -104,30 +163,51 @@ const model = {
       
   }
 };
-// let num=[];
+ function putUpdating(){
+    const input= document.getElementsByTagName("input")[0].value;
+    if(input<=0){
+      alert('Введіть коректні данні, кораблів не може бути 0 або менше');
+    } else if (input>10){
+      alert('Максимальне допустиме значення кораблів 10');
+    }else{
+    startGame.defineNumberShip=input;
+    model.shipRemove(startGame.allPlaceShip);
+    startGame.allPlaceShip=[];
+    model.allShip=[];
+    startGame.defineShips();
+    model.placeShip();
+    model.updateShip();
+    model.findShip(startGame.allPlaceShip);
+    console.log(startGame.allPlaceShip); 
+  }
+ }
+
+
+
+
+
+
+
 startGame.defineShips();
-startGame.defineShips();
-startGame.defineShips();
+// startGame.defineShips();
+// startGame.defineShips();
 
 model.placeShip();
 model.updateShip();
 // for beginner start
-// model.findShip(startGame.firstShip);
-// model.findShip(startGame.secondShip);
-// model.findShip(startGame.thirdShip);
-//end
-
+model.findShip(startGame.allPlaceShip,true);
+//end;
 // alert(startGame.firstShip);
 // alert(startGame.secondShip);
 // alert(startGame.thirdShip);
+// сама логіка подій start
 function onClick(e){
-// e.target.style.backgroundColor='red';
-// e.target.textContent='1';
      if(e.target.textCotent!=='miss'&&e.target.textContent!=='hit'){
         model.counterHits++;
     model.updateHits();
     model.isHit(e.target.id,e);
     model.updateShip();
+
     }
 }
 const td=document.getElementsByClassName('field');
@@ -135,4 +215,9 @@ const td=document.getElementsByClassName('field');
 for (const item of td) {
     item.addEventListener('click',onClick)
 }
-
+//end
+// start події динамічні
+const btn=document.querySelector('.changeNumberShip');
+ 
+btn.addEventListener('click',putUpdating);
+//end
